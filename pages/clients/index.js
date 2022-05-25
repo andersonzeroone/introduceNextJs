3ab0b2637a42bdfdb990032bfdd21b4ef0388a0b
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-25 09:08:27 -0300

    navigation to dynamic route

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 function ClientPage() {
+
+  const clients = [
+    { id: 'max', name: "maxmilian" },
+    { id: 'ander', name: "Anderson" }
+
+  ]
   return (
     <div>
       <h1>Client Page </h1>
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+
+      </ul>
     </div>
   )
 }
