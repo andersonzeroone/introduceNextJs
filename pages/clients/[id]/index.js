43d1cd708f949e0fd682551eb3b3e336bd46beb7
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-25 09:18:48 -0300

    create page notfound

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: '/clients/[id]/[clientprojectId]',
+      query: { id: 'max', clientprojectId: 'projects' }
+    })
+  }
   return (
     <div>
       <h1>Client Projects Page </h1>
+      <button onClick={loadProjectHandler}> Click me</button>
     </div>
   )
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -13,8 +13,18 @@ function ClientPage() {
 
       <ul>
         {clients.map((client) => (
+          // <li key={client.id}>
+          //   <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          // </li>
+
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            <Link href={{
+              pathname: `/clients/[id]`,
+              query: { id: client.id }
+            }
+            }>
+              {client.name}
+            </Link>
           </li>
         ))}
 
